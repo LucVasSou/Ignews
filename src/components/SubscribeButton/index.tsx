ecc1fc821ee9613d0ff5398ce3beebd3744a485f
Lucas Souza <lucasbmbr@hotmail.com>
2022-04-27 11:59:06 -0300

    quarto commit

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,4 @@
-import { signIn, useSession } from 'next/react';
+import { signIn, useSession } from 'next-auth/react';
 import { api } from '../../services/api'
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header/Index';
-import { SessionProvider } from 'next/react'
+import { SessionProvider } from 'next-auth/react'
 
 import '../styles/global.scss';
 
